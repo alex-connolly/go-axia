88728c1c47a6357cebe05d17f92ffc1de1240866
communityalex <ndrpub@gmail.com>
2017-08-21 01:10:14 +1000

    adding string hashing methods

--- axia/hash.go
+++ axia/hash.go
@@ -6,3 +6,28 @@ const (
 
 // Hash ...
 type Hash [hashLength]byte
+
+// HashString ...
+func HashString(str string) (h Hash) {
+	h.SetString(str)
+	return h
+}
+
+// HashBytes ...
+func HashBytes(bytes []byte) (h Hash) {
+	h.SetBytes(bytes)
+	return h
+}
+
+// SetBytes ...
+func (h Hash) SetBytes(bytes []byte) {
+	if len(bytes) > hashLength {
+		bytes = bytes[len(bytes)-hashLength:]
+	}
+	copy(h[hashLength-len(bytes):], bytes)
+}
+
+// SetString ...
+func (h Hash) SetString(str string) {
+	h.SetBytes([]byte(str))
+}
--- axia/hash_test.go
+++ axia/hash_test.go
@@ -1,1 +1,16 @@
 package axia
+
+import "testing"
+
+func TestHashString(t *testing.T) {
+	s := "hello"
+	HashString(s)
+	s = "thisstringislongerthanthehaslengthbecauseitis"
+	HashString(s)
+}
+
+func TestHashBytes(t *testing.T) {
+	s := "hello"
+	HashBytes([]byte(s))
+
+}
